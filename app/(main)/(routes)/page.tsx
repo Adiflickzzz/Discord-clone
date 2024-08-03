fc62ceee92d344a1ec8f4e8f10f1df9f6fb75614
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-08-03 13:46:24 +0530

    dark-theme initiated

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,12 +1,12 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <UserButton 
-      afterSignOutUrl="/"
-      />
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
