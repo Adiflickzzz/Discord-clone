a424e500deb4b2724cd35bddc29a8643aa3b681b
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-08-06 17:05:09 +0530

    Redirecting user back to main page (showing to create a server), if he is has not join any server

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,26 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          //@ts-ignore
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect("/servers/${server.id}");
+  }
+
+  return <div>Create a server</div>;
+};
+
+export default SetupPage;
