0a45273b4094e6775d14399c6d6154684c506d64
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-08-07 18:52:15 +0530

    added a custom classname in css to fix the issue flex and hidded

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,9 +2,11 @@ import { NavigationSidebar } from "@/components/navigations/navigation-sidebar";
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
-      <div className="md:flex w-[70px] z-30 flex-col fixed inset-y-0">
-        <NavigationSidebar />
+    <div className="h-full w-full">
+      <div className="flex w-[70px] z-30 flex-col fixed inset-y-0">
+        <div className="h-full custom-show">
+          <NavigationSidebar />
+        </div>
       </div>
       <main className="md:pl-[70px] h-full">{children}</main>
     </div>
