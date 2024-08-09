414893f408624ec5f84e0f7ed280201b99984a1b
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-08-09 12:26:54 +0530

    Server settings working good

--- components/servers/server-header.tsx
+++ components/servers/server-header.tsx
@@ -50,7 +50,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
 
         {isAdmin && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("settings", { server })}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
             Server Settings
             <Settings className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
